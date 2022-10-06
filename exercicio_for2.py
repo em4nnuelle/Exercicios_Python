5d3f79ae680b6da589f1bc1b622a8dcda0ee05da
em4nnuelle <emannuelle.bs@gmail.com>
2022-10-05 21:25:43 -0300

    exercicios laços de repetição

---
+++ exercicio_for2.py
@@ -0,0 +1,7 @@
+s = 0
+for i in range(5):
+    s += 3*i
+print(s)
+
+#Se o print estiver dentro do laço for, será impresso a sequência de acréscimos.
+#Com o print fora do for, temos apenas o resultado quando o laço se encerra.
